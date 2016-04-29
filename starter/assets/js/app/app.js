66222287ac978693eb8ef48f6f9461831706e352
Shayne Kasai <skasai@corp.thelevel.com>
2016-04-29 07:05:04 -0700

    Refined comments

--- starter/assets/js/app/app.js
+++ starter/assets/js/app/app.js
@@ -1,5 +1,11 @@
 /*
- * Bootstrap any Angular modules to be used by application. For example,
+ * Bootstrap your app with your startApp module. Modules are akin to 
+ * a container for all of the other components (controllers, services,
+ * directives, filters etc.) 
+ *
+ * starterApp is defined as part of the ng-app attribute in index.html
+ *
+ * You can pass in additional modules: For example,
  * if you wanted to use the ngRoute module for routing, you would do:
  *  
  * app = angular.module('starterApp', ['ngRoute']);
